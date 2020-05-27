a70ad6f373e9d526825229e9bbf00e30790c592d
smelizabeth <smelizabeth@yandex.ru>
2020-05-27 17:34:45 +0300

    первый коммит

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const buttonEdit = document.querySelector('.profile__edit-button');
-const popup =  document.querySelector('.popup');
-const nameInput = document.querySelector('.popup__input_name');
-const jobInput = document.querySelector('.popup__input_job');
-const name = document.querySelector('.profile__name');
-const job = document.querySelector('.profile__job');
-const save = document.querySelector('.popup__submit-button');
-const form = document.querySelector('.popup__container');
-const close = document.querySelector('.popup__close-button');
+let buttonEdit = document.querySelector('.profile__edit-button');
+let popup =  document.querySelector('.popup');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
+let name = document.querySelector('.profile__name');
+let job = document.querySelector('.profile__job');
+let save = document.querySelector('.popup__submit-button');
+let form = document.querySelector('.popup__container');
+let close = document.querySelector('.popup__close-button');
 
 function openPopup () {
     popup.classList.add('popup_opened');
@@ -29,3 +29,4 @@ function formSubmitHandler (evt) {
 
 save.addEventListener('click', closePopup);
 form.addEventListener('submit', formSubmitHandler);
+close.addEventListener('click', closePopup)
