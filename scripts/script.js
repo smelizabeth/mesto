49051cac6869a73ba630c6bb80a8a138b9267b8e
smelizabeth <smelizabeth@yandex.ru>
2020-06-01 02:23:34 +0300

    второй коммит

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-let buttonEdit = document.querySelector('.profile__edit-button');
-let popup =  document.querySelector('.popup');
+const buttonEdit = document.querySelector('.profile__edit-button');
+const popup =  document.querySelector('.popup');
 let nameInput = document.querySelector('.popup__input_name');
 let jobInput = document.querySelector('.popup__input_job');
 let name = document.querySelector('.profile__name');
 let job = document.querySelector('.profile__job');
-let save = document.querySelector('.popup__submit-button');
-let form = document.querySelector('.popup__container');
-let close = document.querySelector('.popup__close-button');
+const save = document.querySelector('.popup__submit-button');
+const form = document.querySelector('.popup__container');
+const close = document.querySelector('.popup__close-button');
 
 function openPopup () {
     popup.classList.add('popup_opened');
